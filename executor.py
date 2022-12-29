b45a940ae5a8b7d83a15072173168449aa6548e4
numb3r3 <wangfelix87@gmail.com>
2022-12-29 18:42:25 +0800

    chore: add draft readme

--- executor.py
+++ executor.py
@@ -53,7 +53,9 @@ class WhisperExecutor(Executor):
                 audio = whisper.load_audio(doc.uri)
             elif doc.blob is not None:
                 ext = doc.tags.get('ext', 'wav')
-                with tempfile.NamedTemporaryFile(suffix=f'.{ext}') as fp:
+                with tempfile.NamedTemporaryFile(
+                    suffix=f'.{ext}' if docs.tags.get('ext', None) else None
+                ) as fp:
                     fp.write(doc.blob)
                     fp.flush()
                     audio = whisper.load_audio(fp.name)
@@ -63,7 +65,7 @@ class WhisperExecutor(Executor):
             doc.tensor = whisper.log_mel_spectrogram(audio)
 
     @requests
-    def process(self, docs, **kwargs):
+    def transcribe(self, docs, **kwargs):
         # in encoder mode, only the encoder is used to extract the audio features
         if self._mode.lower() == 'encoder':
             self.load_audio(docs)
@@ -72,7 +74,10 @@ class WhisperExecutor(Executor):
         # in decoder mode, only the decoder is used to transcribe the audio features
         elif self._mode.lower() == 'decoder':
             if docs.embeddings is None:
-                raise ValueError('No embeddings found in the document')
+                raise ValueError(
+                    'No audio embeddings found in the document, '
+                    'please use encoder mode to extract the embeddings first'
+                )
             with torch.inference_mode():
                 result = whisper.decoding.decode(
                     self._model, docs.embeddings, options=self._options
